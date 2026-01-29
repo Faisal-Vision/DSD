457c8387133d9f50a6ab159c7220fca37ddab47e
FaisalShridehh <f_shrideh@yahoo.com>
2026-01-29 13:29:33 +0300

    third

--- Website_DSD.Front/src/main.ts
+++ Website_DSD.Front/src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/*';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(() => {
+    inject();
+    injectSpeedInsights();
+  })
   .catch(err => console.error(err));
